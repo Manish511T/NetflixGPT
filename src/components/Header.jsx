2b99c50072b7774a34172bb5840fc6d2b48ce32d
Manish <mkthakur2301@gmail.com>
2026-02-19 00:50:21 +0530

    add hardcoded to the constants file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constant";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -42,7 +43,7 @@ const Header = () => {
   
   return (
     <div className='absolute  flex justify-between items-center px-8 py-2 w-full bg-linear-to-b from-black z-10'>
-      <img className='w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo' />
+      <img className='w-44' src={LOGO} alt='logo' />
       <div className='flex justify-evenly items-center gap-2'>
         {user && (
           <img
