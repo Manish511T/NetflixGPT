3a960a166bf6fdaf08348239c890ac290b52da78
Manish <mkthakur2301@gmail.com>
2026-02-19 01:41:32 +0530

    get data from TMDB now playing movies list API

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
-import Hero from './Hero'
+import { API_OPTIONS } from '../utils/constant'
+// import Hero from './Hero'
+
 const Browse = () => {
+
+  const getNowPlayingMovies = async ()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+
+    const json =  await data.json();
+    console.log(json);
+  }
+
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  }, [])
+
+
   return (
     <div>
       <Header/>
-      <Hero/>
+      {/* <Hero/> */}
     </div>
   )
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,10 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
-export const USER_AVATAR = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
+export const USER_AVATAR = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDA4Mzc2YWIxMDliNzk5YTNmNTk2OWI4YzA4NDFjYSIsIm5iZiI6MTc3MTQ0MzU5My41NTgsInN1YiI6IjY5OTYxNTg5ODY1ZThlZWU3Yzg2MzQ1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yFnE--G5oCHf4iSiAlng4tjPLCrdDsqGMeSkpl1utGw'
+  }
+};
