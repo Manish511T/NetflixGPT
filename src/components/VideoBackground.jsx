825b8ec3de23c488608577d0b9b1daf21696a343
Manish <mkthakur2301@gmail.com>
2026-02-20 01:40:34 +0530

    improve ui

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -15,14 +15,10 @@ const VideoBackground = ({ movieId }) => {
 
       <iframe
         className="w-full h-full scale-150 pointer-events-none"
-        src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideo.key}&modestbranding=1&showinfo=0`}
+        src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideo.key}&vq=hd720&modestbranding=1&showinfo=0`}
         title="Movie Trailer"
         allow="autoplay; encrypted-media"
       ></iframe>
-
-      <div className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-black via-black/60 to-transparent"></div>
-
-      <div className="absolute bottom-0 w-full h-40 bg-linear-to-t from-black to-transparent"></div>
 
     </div>
   );
